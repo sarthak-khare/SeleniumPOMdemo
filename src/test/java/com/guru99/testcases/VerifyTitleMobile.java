190ef72f8707d561d745ffb7434f77c661044c78
sarthak.k <sarthakkhare2009@gmail.com>
2021-02-07 22:58:32 +0000

    updated pom.xml for maven testng integration

--- src/test/java/com/guru99/testcases/VerifyTitleMobile.java
+++ src/test/java/com/guru99/testcases/VerifyTitleMobile.java
@@ -10,7 +10,7 @@ import com.guru99.pages.MobilePage;
 
 public class VerifyTitleMobile extends ClickOnMobile {
 
-	@Test(priority = 2)
+	@Test(priority = 2, dependsOnMethods = "clickOnMobile")
 	public void verifyMobileTitle() {
 		System.out.println("Inside test2");
 		System.out.println(MobilePage.getTitle());
